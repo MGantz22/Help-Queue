9ca8c160af18787a5c97567352e11d199792183a
Mitchell Gantz <mgantz22@icloud.com>
2023-04-19 15:39:54 -0700

    Update to add functionality

--- src/componets/NewTicketForm.js
+++ src/componets/NewTicketForm.js
@@ -1,11 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
+import ReusableForm from './ReusableForm';
 
-function NewTicketForm(props){
+function NewTicketForm(props) {
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <ReusableForm 
+        formSubmissionHandler={handleNewTicketFormSubmission}
+        buttonText="Help!" />
     </React.Fragment>
   );
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTicketCreation({
+      names: event.target.names.value, 
+      location: event.target.location.value, 
+      issue: event.target.issue.value, 
+      id: v4()
+    });
+  }
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
